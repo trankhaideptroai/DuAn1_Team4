fe51a353b19a08454f0803c4d47ae9739e59b69d
Akito0310 <bachubucha@gmail.com>
2024-07-23 11:45:22 +0700

    Add files via upload

---
+++ DAL/DataProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    internal class DataProvider
+    {
+        public static string connectionstr = @"Data Source=DESKTOP-4EF68B5\TUAN;Initial Catalog=database_duan1;Integrated Security=True";
+        public DataTable ExcuteQuery(string querry)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connnection = new SqlConnection(connectionstr))
+            {
+                connnection.Open();
+                SqlCommand command = new SqlCommand(querry, connnection);
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.Fill(data);
+                connnection.Close();
+
+            }
+            return data;
+        }
+
+        public static SqlConnection connect()
+        {
+            return new SqlConnection(connectionstr);
+        }
+    }
+}
