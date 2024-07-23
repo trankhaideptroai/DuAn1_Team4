5f7c385fa7f1b7f8c9fbae105bb67f2aa70bc100
Akito0310 <bachubucha@gmail.com>
2024-07-23 11:44:14 +0700

    Add files via upload

---
+++ BUS/Interface/INhapKho_BUS.cs
@@ -0,0 +1,27 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.Interface
+{
+    public interface INhapKho_BUS
+    {
+        #region source
+        DataTable Load();
+
+        bool Insert(NhapKho_DTO sp);
+
+        bool Delete(string masp);
+
+        bool Update(NhapKho_DTO sp);
+
+        DataTable Find(string tensp);
+
+        #endregion
+    }
+}
