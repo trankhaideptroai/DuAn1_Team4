87f922ed72501045dc5c290bda972ae6b0cc0518
Akito0310 <bachubucha@gmail.com>
2024-07-23 11:48:36 +0700

    Add files via upload

---
+++ DTO/NhapKho_DTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class NhapKho_DTO : AbsNhapKho_DTO
+    {
+
+    }
+}
