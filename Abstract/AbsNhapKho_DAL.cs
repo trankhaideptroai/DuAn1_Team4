2c9a7b652bc54f4ba1b55bce93633140206d5aee
Akito0310 <bachubucha@gmail.com>
2024-07-23 19:44:58 +0700

    Add files via upload

---
+++ Abstract/AbsNhapKho_DAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Abstract
+{
+    public abstract class AbsNhapKho_DAL
+    {
+        public abstract bool Execute(string proc, params SqlParameter[] para);
+    }
+}
---
+++ NhapKho_BUS.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL;
+using BUS.Interface;
+using System.Data;
+
+namespace BUS
+{
+    public class NhapKho_BUS : INhapKho_BUS
+    {
+        NhapKho_DAL sp2 = new NhapKho_DAL();
+
+        #region source
+
+        // Tải danh sách
+        public DataTable Load()
+        {
+            return sp2.loadlist();
+        }
+
+        // Thêm
+        public bool Insert(NhapKho_DTO sp)
+        {
+            return sp2.insert(sp);
+        }
+
+        // Xóa
+        public bool Delete(string masp)
+        {
+            return sp2.delete(masp);
+        }
+
+        // Sửa
+        public bool Update(NhapKho_DTO sp)
+        {
+            return sp2.update(sp);
+        }
+
+        // Tìm kiếm
+        public DataTable Find(string tensp)
+        {
+            return sp2.finding(tensp);
+        }
+
+#endregion
+    }
+}
